3d8ee503fdb3f0bf19d5c009390707bdcd383094
javi11292 <javi_1_12_92@hotmail.com>
2018-10-21 15:46:16 +0200

    update

---
+++ app/src/components/App/index.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+import App from "./App"
+
+class AppContainer extends PureComponent {
+
+  render() {
+    return <App />
+  }
+
+}
+
+export default AppContainer
---
+++ server/src/index.js
@@ -0,0 +1,12 @@
+import express from "express"
+import io from "socket.io"
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+const server = io(app.listen(PORT))
+
+server.on("connection", socket => {
+    console.log("User connected")
+})
