08619e714bc15c4e4df326fea678840ea8adf378
jroblefe <javier.robles.fernandez.sa@everis.com>
2018-10-30 15:49:49 +0100

    update

--- app/src/components/App/index.js
+++ app/src/components/App/index.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react'
+import { CssBaseline } from "@material-ui/core"
 
 import Store from "components/Store"
 import App from "./App"
@@ -6,6 +7,7 @@ import App from "./App"
 class AppContainer extends PureComponent {
     render = () => (
         <Store>
+            <CssBaseline />
             <App />
         </Store>
     )
