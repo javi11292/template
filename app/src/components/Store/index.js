c45d0110b33db25954f936c1d01929d800fb65f0
jroblefe <javier.robles.fernandez.sa@everis.com>
2018-11-15 08:48:32 +0100

    update

--- app/src/components/Store/index.js
+++ app/src/components/Store/index.js
@@ -12,11 +12,8 @@ const actions = {
 }
 
 class Store extends React.PureComponent {
-    constructor(props) {
-        super(props)
-        this.state = {
+    state = {
 
-        }
     }
 
     render = () => (
