ff23041a0162d835a36c78897eafb368ae69423c
jroblefe <javier.robles.fernandez.sa@everis.com>
2018-11-16 10:18:55 +0100

    update

--- app/src/components/Main/index.js
+++ app/src/components/Main/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import { CssBaseline } from "@material-ui/core"
 
-import Store from "components/Store"
 import Main from "./Main"
 
 class MainContainer extends React.PureComponent {
