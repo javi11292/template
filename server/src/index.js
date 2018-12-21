72502650cdc51c183786439ad890d4b2de745f1f
jroblefe <javier.robles.fernandez.sa@everis.com>
2018-12-21 14:01:55 +0100

    update

--- server/src/index.js
+++ server/src/index.js
@@ -2,8 +2,11 @@
 
 const express = require("express")
 
+const router = require("./router")
+
 const PORT = process.env.PORT || 80
 const app = express()
 const onReady = () => console.log(`Listening on port ${PORT}`)
 
+app.use(router)
 app.listen(PORT, onReady)
