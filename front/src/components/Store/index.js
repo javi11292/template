7164c4bf2aebc9f7b208d63e4cb4f4dfd2245258
Javi <javi_1_12_92@hotmail.com>
2019-02-12 08:50:12 +0100

    Fix context

--- front/src/components/Store/index.js
+++ front/src/components/Store/index.js
@@ -1,23 +1,27 @@
-import React, { createContext, useState, useRef } from "react"
+import React, { createContext, useState, useRef, useContext } from "react"
 import store from "./store"
 
-const context = Object.keys(store).reduce(addContexts, {})
+const contexts = Object.keys(store).reduce(addContexts, { providers: {}, consumers: {} })
 const Providers = Object.entries(store).reduce(addProviders, null) || (({ component }) => component)
 
 const Store = props => <Providers component={props.children} />
 
 function addContexts(acc, key) {
-    return { ...acc, [key]: createContext() }
+    const context = createContext()
+    return {
+        providers: { ...acc.providers, [key]: context.Provider },
+        consumers: { ...acc.consumers, [`use${key.slice(0, 1).toUpperCase()}${key.slice(1)}`]: () => useContext(context) }
+    }
 }
 
 function addProviders(Acc, [key, entry]) {
-    const Provider = context[key].Provider
+    const Provider = contexts.providers[key]
     return ({ component }) => {
         const [state, setState] = useState(entry.state)
         const actions = useRef(entry.actions(setState))
-        return <Provider value={{ [key]: state, ...actions.current }}>{Acc ? <Acc component={component} /> : component}</Provider>
+        return <Provider value={{ state, ...actions.current }}>{Acc ? <Acc component={component} /> : component}</Provider>
     }
 }
 
 export default Store
-export { context }
+export const context = contexts.consumers
