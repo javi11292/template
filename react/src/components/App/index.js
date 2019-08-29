3796d0ecb735affc88f3a9ce069504afee51cd71
Javier Robles Fernández <javi_1_12_92@hotmail.com>
2019-08-29 15:29:29 +0200

    Refactor

--- react/src/components/App/index.js
+++ react/src/components/App/index.js
@@ -2,19 +2,11 @@ import React from "react"
 import { CssBaseline } from "@material-ui/core"
 import { Store } from "eztore"
 import reducers from "reducers"
-import useStyles from "./useStyles"
 
-const App = props => {
-    const classes = useStyles(props)
-    
-    return (
-        <Store reducers={reducers}>
-            <CssBaseline />
-            <div className={classes.root}>
-
-            </div>
-        </Store>
-    )
-}
+const App = () => (
+    <Store reducers={reducers}>
+        <CssBaseline />
+    </Store>
+)
 
 export default App
