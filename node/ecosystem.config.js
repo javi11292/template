76f6100530ef3add8625398de78ceab921c82541
Javier Robles Fernández <javi_1_12_92@hotmail.com>
2019-09-05 16:32:53 +0200

    Fix env

--- node/ecosystem.config.js
+++ node/ecosystem.config.js
@@ -2,7 +2,8 @@ module.exports = {
     apps: [{
         name: "node",
         script: 'index.js',
-        "log_date_format" : "DD/MM/YY HH:mm:ss",
+        "log_date_format": "DD/MM/YY HH:mm:ss",
         instances: "max",
+        node_args: "-r dotenv/config",
     }],
 }
