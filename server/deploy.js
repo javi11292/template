9f1960171109d61da7c21eba1df7dd315921fa47
Javier Robles Fernández <javi_1_12_92@hotmail.com>
2019-09-27 12:38:18 +0200

    Refactor

--- server/deploy.js
+++ server/deploy.js
@@ -4,5 +4,5 @@ const { spawnSync } = require("child_process")
 
 const name = process.env.npm_package_name
 
-spawnSync(`heroku container:push web -a javiscript-${name}`, { stdio: "inherit" })
-spawnSync(`heroku container:release web -a javiscript-${name}`, { stdio: "inherit" })
+spawnSync(`heroku container:push web -a javiscript-${name}`, { stdio: "inherit", shell: true })
+spawnSync(`heroku container:release web -a javiscript-${name}`, { stdio: "inherit", shell: true })
