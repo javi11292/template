880404ff9c401e75b011c71acae138483c7a9c0f
Javier Robles Fernández <javi_1_12_92@hotmail.com>
2019-09-27 14:58:35 +0200

    Refactor

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
     apps: [{
         name: "server",
-        script: 'index.js',
+        script: "index.js",
         "log_date_format": "DD/MM/YY HH:mm:ss",
         instances: "max",
         node_args: "-r dotenv/config",
     }, {
         name: "wake",
-        script: 'wake.js',
+        script: "wake.js",
         instances: "1",
     }],
 }
