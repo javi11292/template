7e600a0f408edab377e873b13600c2af98008ba8
Javier Robles Fernández <javi_1_12_92@hotmail.com>
2019-10-29 12:21:17 +0100

    Refactor

--- server/index.js
+++ server/index.js
@@ -4,9 +4,5 @@ const router = require("./src/router")
 const PORT = process.env.PORT
 const app = express()
 
-function onReady() {
-  console.log(`Listening on port ${PORT}`)
-}
-
 app.use(router)
-app.listen(PORT, onReady)
+app.listen(PORT)
