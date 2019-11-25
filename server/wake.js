f90e64133c1bb9fb2dbf9e3f09d74c841c8e9434
Javier Robles Fernández <javi_1_12_92@hotmail.com>
2019-11-25 15:11:31 +0100

    Refactor

--- server/wake.js
+++ server/wake.js
@@ -1,5 +1,3 @@
 const https = require("https")
 
-setInterval(() => {
-  https.get(`https://${process.env.npm_package_name}.herokuapp.com`)
-}, 15 * 60 * 1000)
+setInterval(() => https.get(`https://${process.env.npm_package_name}.herokuapp.com`), 15 * 60 * 1000)
