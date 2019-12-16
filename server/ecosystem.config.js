34977bce82850658cfde482622973fe3796f1a53
Javier Robles Fernández <javi_1_12_92@hotmail.com>
2019-12-16 10:45:22 +0100

    Refactor

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,9 +1,8 @@
 module.exports = {
   apps: [{
     name: "server",
-    script: "index.js",
+    script: "src",
     instances: "max",
-    node_args: "-r dotenv/config",
     "log_date_format": "DD/MM/YY HH:mm:ss",
   }, {
     name: "wake",
--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,10 @@
+require("./env")
 const express = require("express")
-const router = require("./src/router")
+const router = require("./router")
 
 const PORT = process.env.PORT
 const app = express()
 
 app.use(router)
+
 app.listen(PORT)
