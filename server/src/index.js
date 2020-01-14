fad0e678bc49f0b2bd45eb187a2d333648751748
Javier Robles Fernández <javi_1_12_92@hotmail.com>
2020-01-14 11:00:15 +0100

    Refactor

--- server/src/index.js
+++ server/src/index.js
@@ -5,7 +5,7 @@ const app = express()
 
 app.use(compression())
 app.use(express.static("/client", { maxAge: 1000 * 60 * 60 * 24 * 365 }))
-app.get("*", (req, res) => res.sendFile("/client/index.html"))
+app.use((req, res) => res.sendFile("/client/index.html"))
 
 app.listen(3000, () => {
   console.log("Server started")
