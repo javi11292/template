3795823adbec8e0ebc17704bea35533c87a666c5
Javier Robles Fernández <javi_1_12_92@hotmail.com>
2020-01-31 11:28:43 +0100

    Refactor

--- client/src/components/Notifications/useStyles.js
+++ client/src/components/Notifications/useStyles.js
@@ -1,7 +1,7 @@
 import React from "react"
+import styled from "styled-components"
 import { SnackbarContent as MuiSnackbarContent } from "@material-ui/core"
 import { NOTIFICATION } from "libraries/constants"
-import styled from "styled-components"
 
 export const SnackbarContent = styled(({ variant, ...props }) => <MuiSnackbarContent {...props} />)`
   background-color: ${props => props.variant === NOTIFICATION.error ? props.theme.palette.error.dark : props.theme.palette.primary.dark};
