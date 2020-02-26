1e0e0d004fac20e8e78ef463b5e297eb2b359706
Javier Robles Fernández <javi_1_12_92@hotmail.com>
2020-02-26 18:49:11 +0100

    refactor

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,27 @@
 const express = require("express")
 const router = require("./router")
+const https = require("https")
+const http = require("http")
+const fs = require("fs")
+
+function getOptions() {
+  try {
+    return {
+      key: fs.readFileSync(process.env.KEY),
+      cert: fs.readFileSync(process.env.CERT),
+    }
+  } catch  { }
+}
 
 const app = express()
+const options = getOptions()
 
 app.use(router)
 
-app.listen(3000, () => console.log("Server started"))
+if (options) {
+  https.createServer(options, app).listen(3443, () => console.log("HTTPS server started"))
+} else {
+  http.createServer(app).listen(3080, () => console.log("HTTP server started"))
+}
 
 process.on("SIGTERM", process.exit)
