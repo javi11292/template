bac624521960e2bb3a1d074df463600ea9a66124
Javier Robles Fernández <javi_1_12_92@hotmail.com>
2020-03-26 14:09:35 +0100

    Refactor

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,7 @@
 import React from "react"
 import { hydrate, render } from "react-dom"
-import WebFont from "webfontloader"
 import App from "components/App"
 import * as serviceWorker from "serviceWorker"
-
-function loadFont() {
-  WebFont.load({ google: { families: ["Roboto:300,400,500,700&display=swap"] } })
-}
 
 function onUpdate(registration) {
   window.dispatchEvent(new CustomEvent("update", { detail: registration.waiting }))
@@ -14,13 +9,10 @@ function onUpdate(registration) {
 
 const rootElement = document.getElementById("root");
 
-if (process.env.NODE_ENV === "production" && rootElement.hasChildNodes()) {
+if (rootElement.hasChildNodes()) {
   Array.from(document.getElementsByTagName("style")).forEach(style => style.remove())
-  loadFont()
   hydrate(<App />, rootElement);
+  serviceWorker.register({ onUpdate })
 } else {
-  if (process.env.NODE_ENV === "development") loadFont()
   render(<App />, rootElement);
 }
-
-serviceWorker.register({ onUpdate })
