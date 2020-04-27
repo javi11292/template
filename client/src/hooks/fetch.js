6c069105353ca3689fcc4bbde0952593f9d2dee5
Javier Robles Fernández <javi_1_12_92@hotmail.com>
2020-04-27 22:24:10 +0200

    Refactor

--- client/src/hooks/fetch.js
+++ client/src/hooks/fetch.js
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react"
 import { get, post } from "libraries/fetch"
 
-export function useFetch([path, body]) {
+export function useFetch(request) {
   const [loading, setLoading] = useState(false)
   const [response, setResponse] = useState()
 
   useEffect(() => {
+    const [path, body] = request
     if (path) {
       async function getResponse() {
         const response = await (body ? post(path, body) : get(path))
@@ -17,7 +18,7 @@ export function useFetch([path, body]) {
       setResponse()
       setLoading(true)
     }
-  }, [path, body])
+  }, [request])
 
   return { response, loading }
 }
