c88a1a9ca071b3a2b326fabecb1e952035d01a65
Javier Robles Fernández <javi_1_12_92@hotmail.com>
2020-05-01 16:29:00 +0200

    Refactor

--- client/next.config.js
+++ client/next.config.js
@@ -1,20 +1,22 @@
 const withPWA = require("next-offline")
 
-module.exports = withPWA({
-  dontAutoRegisterSw: true,
-  workboxOpts: {
-    swDest: `${__dirname}/public/service-worker.js`,
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: "NetworkFirst",
-        options: {
-          cacheName: "offlineCache",
-          expiration: {
-            maxEntries: 200
+module.exports = withPWA(
+  {
+    dontAutoRegisterSw: true,
+    workboxOpts: {
+      swDest: `${__dirname}/public/service-worker.js`,
+      runtimeCaching: [
+        {
+          urlPattern: /^https?.*/,
+          handler: "CacheFirst",
+          options: {
+            cacheName: "offlineCache",
+            expiration: {
+              maxEntries: 200
+            }
           }
         }
-      }
-    ]
+      ]
+    }
   }
-})
+)
