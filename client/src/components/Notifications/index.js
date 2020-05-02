6512214851ccbb2982efd6f3c9d01f7fcb22f3ba
Javier Robles Fernández <javi_1_12_92@hotmail.com>
2020-05-02 10:32:45 +0200

    Refactor

--- client/src/components/Notifications/index.js
+++ client/src/components/Notifications/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import { useState, useEffect } from "react"
 import { Snackbar, SnackbarContent } from "@material-ui/core"
 import { useStore } from "hooks/store"
 
