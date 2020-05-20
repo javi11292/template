9c2ff2bbe320d2b3d8b1201b748f2317a9af975b
Javier Robles Fernández <javi_1_12_92@hotmail.com>
2020-05-20 13:01:29 +0200

    Refactor

--- client/next.config.js
+++ client/next.config.js
@@ -1,5 +1,5 @@
 const withPWA = require("next-offline")
-const withBundleAnalyzer = require('@next/bundle-analyzer')({ enabled: process.env.ANALYZE === 'true' })
+const withBundleAnalyzer = require("@next/bundle-analyzer")({ enabled: process.env.ANALYZE === "true" })
 
 const VERSION = process.env.npm_package_version
 
