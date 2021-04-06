b78f61dffd37ab4fd7b00518ff8cce68421a7ddb
= <=>
2021-04-06 16:36:09 +0200

    Refactor

--- client/src/pages/_document.js
+++ client/src/pages/_document.js
@@ -14,11 +14,11 @@ export default class Document extends NextDocument {
           <link rel="icon" href="/favicon.ico" />
           <link rel="apple-touch-icon" href="/logo192.png" />
           <link rel="manifest" href="/manifest.json" />
-          <link href="https://storage.googleapis.com" rel="preconnect" />
-          <link href="https://fonts.googleapis.com" rel="preconnect" />
           <link href="https://fonts.gstatic.com" rel="preconnect" />
           <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="preload" as="style" />
           <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="preload" as="style" />
+          <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
         </Head>
 
         <body>
