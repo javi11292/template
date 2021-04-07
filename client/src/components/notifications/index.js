f639ad39b9e3feb8436ca517a432a4ba65db6a06
= <=>
2021-04-07 13:23:09 +0200

    Fix notification

--- client/src/components/notifications/index.js
+++ client/src/components/notifications/index.js
@@ -1,14 +1,21 @@
+import { useEffect, useState } from 'react';
+
 import { useStore } from 'hooks/store';
 import Snackbar from 'ui/snackbar';
 
 export default function Notifications() {
   const [notifications, dispatchNotifications] = useStore('notifications');
+  const [open, setOpen] = useState(false);
+  const notification = notifications[0];
 
   function handleExited() {
+    setOpen(false);
     dispatchNotifications({ type: 'shift' });
   }
 
-  const notification = notifications[0];
+  useEffect(() => {
+    setOpen(!!notification);
+  }, [notification]);
 
-  return <Snackbar onExit={handleExited} open={!!notification}>{notification}</Snackbar>;
+  return <Snackbar onExit={handleExited} open={open}>{notification}</Snackbar>;
 }
