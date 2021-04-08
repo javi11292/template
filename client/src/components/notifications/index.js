ea0e0516b27fabff083076fdd3aa9f4d9595c1bd
= <=>
2021-04-08 20:07:01 +0200

    Refactor

--- client/src/components/notifications/index.js
+++ client/src/components/notifications/index.js
@@ -4,13 +4,13 @@ import { useStore } from 'hooks/store';
 import Snackbar from 'ui/snackbar';
 
 export default function Notifications() {
-  const [notifications, dispatchNotifications] = useStore('notifications');
+  const [notifications, setNotifications] = useStore('notifications');
   const [open, setOpen] = useState(false);
   const notification = notifications[0];
 
   function handleExited() {
     setOpen(false);
-    dispatchNotifications({ type: 'shift' });
+    setNotifications({ type: 'shift' });
   }
 
   useEffect(() => {
